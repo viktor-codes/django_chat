a64e34d6c887775f7b9916c8440314281ad79688
Viktor Rura <ruravictor@gmail.com>
2024-06-19 21:36:46 +0100

    Added single room template and implement the button join the room.

--- room/views.py
+++ room/views.py
@@ -7,3 +7,9 @@ from .models import Room
 def rooms(request):
     rooms = Room.objects.all()
     return render(request, "room/rooms.html", {"rooms": rooms})
+
+
+@login_required
+def room(request, slug):
+    room = Room.objects.get(slug=slug)
+    return render(request, "room/room.html", {"room": room})
