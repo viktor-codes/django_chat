36586b990d7acdaff2b80f51aedd88678d32490a
Viktor Rura <ruravictor@gmail.com>
2024-06-19 22:00:02 +0100

    Fixed consumers.py, created routing and configured asgi.py

--- room/consumers.py
+++ room/consumers.py
@@ -10,14 +10,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.room_group_name = "chat_%s" % self.room_name
 
         await self.channel_layer.group_add(
-            self.room_name,
-            self.room_group_name
+            self.room_group_name,
+            self.channel_name
         )
 
         await self.accept()
 
     async def disconnect(self):
         await self.channel_layer.group_discard(
-            self.room_name,
-            self.room_group_name
+            self.room_group_name,
+            self.channel_name
         )
